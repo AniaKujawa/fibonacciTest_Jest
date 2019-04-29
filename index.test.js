d831b6a53f35c217fbde06f1c5dfe714ff1a77ae
Ania Kujawa <kujawanna@gmail.com>
2019-04-29 21:44:11 +0200

    comments

--- index.test.js
+++ index.test.js
@@ -1,5 +1,6 @@
 const fib = require('./index');
 
+//Sprawdzam pierwsze "wyjątkowe wartości"
 test('Fib 1 gives 1 ', () => {
     expect(fib(1)).toEqual([1]);
   });
@@ -12,10 +13,12 @@ test('Fib 1 gives 1 ', () => {
     expect(fib(2)).toEqual([1]);
   });
 
+  //Sprawdzam niewłaściwy input
   test('Fib with not number argument gives statement ', () => {
       expect(fib('piesel')).toEqual('You must enter a positive number')
   })
 
+  //Sprawdzam właściwy input
   test('Fib with arg > 2 gives correct value ', () => {
     expect(fib(14)).toEqual([377])
 })
